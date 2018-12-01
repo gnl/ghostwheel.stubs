d5098a3f4d41d3b576e2c585888310f489564fb8
George Lipov <gnl@lipov.net>
2018-12-01 13:43:01 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject gnl/ghostwheel.stubs "0.3.0-SNAPSHOT"
+  :description "Production stubs for Ghostwheel"
+  :url "https://github.com/gnl/ghostwheel.stubs"
+  :scm {:name "git"
+        :url  "https://github.com/gnl/ghostwheel.stubs"}
+  :license {:name "Eclipse Public License"
+            :url  "https://choosealicense.com/licenses/epl-2.0/"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
+  :dependencies [[gnl/ghostwheel.unghost "0.3.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.439"]
+                                  [org.clojure/clojure "1.9.0"]]}}
+  :source-paths ["src"]
+  :clean-targets ^{:protect false} ["target" "resources"])
+
---
+++ src/ghostwheel/core.cljc
@@ -0,0 +1,24 @@
+(ns ghostwheel.core
+  (:require [ghostwheel.unghost :refer [clean-defn]]))
+
+(def => :ret)
+(def | :st)
+(def <- :gen)
+
+(defmacro >defn [& forms] (clean-defn 'defn forms))
+(defmacro >defn- [& forms] (clean-defn 'defn- forms))
+
+(defn- cljs-env? [env] (boolean (:ns env)))
+
+(defmacro ?
+  [& forms]
+  `(~(if (cljs-env? &env)
+       'cljs.spec.alpha/nilable
+       'clojure.spec.alpha/nilable)
+    ~@forms))
+
+(defmacro >fdef [& _] nil)
+(defmacro after-check [& _] nil)
+(defmacro check [& _] nil)
+
+
